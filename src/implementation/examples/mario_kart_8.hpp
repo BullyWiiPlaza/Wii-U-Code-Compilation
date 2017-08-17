6bdd3623af7d1a2a11b81492ecd7306352c61056
BullyWiiPlaza <bullywiihacks@gmail.com>
2017-08-17 14:59:51 +0200

    Add Mario Kart 8 example

---
+++ src/implementation/examples/mario_kart_8.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+// TODO Choose any address you want (should have value 0 at first)
+#define RANDOM_NUMBER_SEED_ADDRESS 0x12345678
+#define VR_ADDRESS 0x2F748660
+
+static unsigned long *randomNumberSeed = (unsigned long *) RANDOM_NUMBER_SEED_ADDRESS;
+
+static inline int getRandomNumber() {
+	*randomNumberSeed = *randomNumberSeed * 1103515245 + 12345;
+	return (unsigned int) (*randomNumberSeed / 65536) % 32768;
+}
+
+static inline void setRandomNumberSeed(unsigned int seed) {
+	*randomNumberSeed = seed;
+}
+
+static inline int random_number(int min_num, int max_num) {
+	int result = 0, low_num = 0, hi_num = 0;
+
+	if (min_num < max_num) {
+		low_num = min_num;
+		hi_num = max_num + 1; // include max_num in output
+	} else {
+		low_num = max_num + 1; // include max_num in output
+		hi_num = min_num;
+	}
+
+	result = (getRandomNumber() % (hi_num - low_num)) + low_num;
+
+	return result;
+}
+
+__attribute__((noinline))
+void randomVR() {
+	if (*randomNumberSeed == 0) {
+		setRandomNumberSeed(1337);
+	}
+	unsigned int random = (unsigned int) random_number(0, 10000);
+	*(unsigned int *) VR_ADDRESS = random;
+}
